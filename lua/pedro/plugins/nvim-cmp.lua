8f3b601db2585d8707a92b7ad950a2f3796faed3
pedrovalido <pedro.validex@gmail.com>
2023-02-17 08:44:55 +0000

    tab functionality uncomment

--- lua/pedro/plugins/nvim-cmp.lua
+++ lua/pedro/plugins/nvim-cmp.lua
@@ -22,10 +22,10 @@ require("luasnip/loaders/from_vscode").lazy_load()
 vim.opt.completeopt = "menu,menuone,noselect"
 
 -- helper function for super tab functionality (not in youtube nvim video)
--- local has_words_before = function()
--- 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
--- 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
--- end
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
 	snippet = {
@@ -43,17 +43,17 @@ cmp.setup({
 		["<CR>"] = cmp.mapping.confirm({ select = false }),
 
 		-- super tab functionality (not in youtube nvim video)
-		-- ["<Tab>"] = cmp.mapping(function(fallback) -- use tab for next suggestion
-		-- 	if cmp.visible() then
-		-- 		cmp.select_next_item()
-		-- 	elseif luasnip.expand_or_jumpable() then
-		-- 		luasnip.expand_or_jump()
-		-- 	elseif has_words_before() then
-		-- 		cmp.complete()
-		-- 	else
-		-- 		fallback()
-		-- 	end
-		-- end, { "i", "s" }),
+		["<Tab>"] = cmp.mapping(function(fallback) -- use tab for next suggestion
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 		--
 		-- ["<S-Tab>"] = cmp.mapping(function(fallback) -- use shift-tab for prev suggestion
 		-- 	if cmp.visible() then
