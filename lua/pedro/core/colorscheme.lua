1fa4490b9b91e994c36758a100260c0d83dba61c
pedrovalido <pedro.validex@gmail.com>
2023-05-24 20:40:36 +0100

    Define gruvbox as default colorscheme, delete commented ones

--- lua/pedro/core/colorscheme.lua
+++ lua/pedro/core/colorscheme.lua
@@ -1,6 +1,4 @@
--- local status, _ = pcall(vim.cmd, "colorscheme nightfly")
 local status, _ = pcall(vim.cmd, "colorscheme gruvbox")
--- local status, _ = pcall(vim.cmd, "colorscheme github_light_default") -- light theme for bright days -- check https://github.com/projekt0n/github-nvim-theme for more themes
 
 if not status then
 	print("colorscheme not found!")
