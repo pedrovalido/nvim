1ddb1d65dfea070c5dcc9fdda4976024201f6a31
pedrovalido <pedro.validex@gmail.com>
2023-06-02 09:52:45 +0100

    Stop cursor from moving horizontally on changed keymap

--- lua/pedro/core/keymaps.lua
+++ lua/pedro/core/keymaps.lua
@@ -21,8 +21,8 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
 
-keymap.set("v", "J", " :m '>+1<CR>gv=gv") -- move selected line together
-keymap.set("v", "K", " :m '<-2<CR>gv=gv") -- move selected line together
+keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move selected line together
+keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move selected line together
 
 keymap.set("x", "<leader>p", '"_dP') -- paste on top selected word without copying deleted word : theprimeagen
 
