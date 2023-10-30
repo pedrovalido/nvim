e78295158bb1956cf50f2b3444e4356bf95b42a7
pedrovalido <pedro.validex@gmail.com>
2023-10-30 09:23:22 +0000

    fix: small identation error

--- lua/pedro/core/keymaps.lua
+++ lua/pedro/core/keymaps.lua
@@ -46,7 +46,7 @@ keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-keymap.set("n", "<leader>fa", "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>") -- find string in all files including hidden files
+keymap.set("n", "<leader>fa", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true <CR>") -- find file in all files including hidden files
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>ft", "<cmd>Telescope colorscheme<cr>") -- list available colorschemes
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
