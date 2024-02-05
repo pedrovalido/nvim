39585e04663140f6bda3acd81689ec0a66312a63
pedrovalido <pedro.validex@gmail.com>
2024-02-05 14:07:06 +0100

    feat: use nomicfoundation for solitidy

--- lua/pedro/plugins/lsp/mason.lua
+++ lua/pedro/plugins/lsp/mason.lua
@@ -11,8 +11,13 @@ if not mason_lspconfig_status then
 end
 
 -- import mason-null-ls plugin safely
-local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
-if not mason_null_ls_status then
+-- local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+-- if not mason_null_ls_status then
+-- 	return
+-- end
+
+local lspconfig_status, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status then
 	return
 end
 
@@ -30,11 +35,20 @@ mason_lspconfig.setup({
 		"bashls", --bash
 		"jsonls", --json
 		"jedi_language_server", --python
-		"solang", --solidity (there are others)
+		-- "solang", --solidity (there are others)
+		"solidity_ls_nomicfoundation", --solidity (there are others)
 		-- "yamlls", --yaml
 	},
 	-- auto-install configured servers (with lspconfig)
 	-- automatic_installation = true, -- not the same as ensure_installed
+})
+
+lspconfig.solidity_ls_nomicfoundation.setup({
+	cmd = { "nomicfoundation-solidity-language-server", "--stdio" },
+	filetypes = { "solidity" },
+	-- root_dir = lspconfig.util.find_git_ancestor,
+	require("lspconfig.util").root_pattern("foundry.toml"),
+	single_file_support = true,
 })
 
 -- mason_null_ls.setup({
