52304f66a90135eb93821d6bb0a78caa9c4ac094
pedrovalido <pedro.validex@gmail.com>
2024-04-10 13:23:24 +0200

    refactor: fix hint symbol, useful imports, remove redundant comments

--- lua/pedro/plugins/lsp/lspconfig.lua
+++ lua/pedro/plugins/lsp/lspconfig.lua
@@ -16,7 +16,7 @@ if not typescript_setup then
 	return
 end
 
-local keymap = vim.keymap -- for conciseness
+local keymap = vim.keymap
 
 -- enable keybinds only for when lsp server available
 local on_attach = function(client, bufnr)
@@ -40,8 +40,8 @@ local on_attach = function(client, bufnr)
 	-- typescript specific keymaps (e.g. rename file and update imports)
 	if client.name == "tsserver" then
 		keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
-		-- keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
-		-- keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
+		keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports
+		keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables
 	end
 end
 
@@ -49,8 +49,7 @@ end
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
 -- Change the Diagnostic symbols in the sign column (gutter)
--- (not in youtube nvim video)
-local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
@@ -69,18 +68,6 @@ typescript.setup({
 		on_attach = on_attach,
 	},
 })
-
--- configure css server
--- lspconfig["cssls"].setup({
--- 	capabilities = capabilities,
--- 	on_attach = on_attach,
--- })
-
--- configure tailwindcss server
--- lspconfig["tailwindcss"].setup({
--- 	capabilities = capabilities,
--- 	on_attach = on_attach,
--- })
 
 --configure helm ls
 -- lspconfig["helm_ls"].setup({
